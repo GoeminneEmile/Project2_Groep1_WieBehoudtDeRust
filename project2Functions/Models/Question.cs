6bdfc26c5e69f55c6ea0acafc13f2785470f45db
nick <nick.langens@student.howest.be>
2020-01-06 15:35:54 +0100

    receive questions

---
+++ project2Functions/Models/Question.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace project2Functions.Models
+{
+    public class Question
+    {
+        public string QuestionName;
+    }
+}

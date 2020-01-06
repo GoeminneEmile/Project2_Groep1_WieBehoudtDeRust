385074bf7ab6d8d9be00750288133aefada8b564
Robbe Vam den Daele <robbe.van.den.daele@student.howest.be>
2020-01-06 12:33:18 +0100

    Toevoegen van een script dat de makey makey test

--- Python/Test Makey Makey.py
+++ Python/Test Makey Makey.py
@@ -1,1 +1,11 @@
-# Test
+# ---- Test Van de Makey Makey ----
+import keyboard
+
+while True:
+    try:
+        if keyboard.is_pressed('W'):
+            print('You Pressed W Key!')
+            break
+    except:
+        break
+

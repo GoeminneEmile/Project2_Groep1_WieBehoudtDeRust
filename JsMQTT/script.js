3455cc049f3aed0e4f720008907c6da9b3933771
Luka De Bakker <Luka.de.bakker@student.howest.be>
2020-01-07 09:09:35 +0100

    Finished general MQTT Js client!

--- JsMQTT/script.js
+++ JsMQTT/script.js
@@ -1,57 +1,35 @@
-// Called after form input is processed
-function startConnect() {
-	// Generate a random client ID
-	clientID = 'clientID-' + parseInt(Math.random() * 100);
+// Create a client instance
+let topic = '/test/luka';
+let clientID = 'clientID_' + parseInt(Math.random() * 100);
+let client = new Paho.Client('/mct-mqtt.westeurope.cloudapp.azure.com', 80, clientID);
+console.log('client made');
 
-	// Fetch the hostname/IP address and port number from the form
-	host = document.getElementById('host').value;
-	port = document.getElementById('port').value;
+// set callback handlers
+client.onConnectionLost = onConnectionLost;
+client.onMessageArrived = onMessageArrived;
+console.log('handlers set');
 
-	// Print output for the user in the messages div
-	document.getElementById('messages').innerHTML += '<span>Connecting to: ' + host + ' on port: ' + port + '</span><br/>';
-	document.getElementById('messages').innerHTML += '<span>Using the following client value: ' + clientID + '</span><br/>';
+// connect the client
+client.connect({ onSuccess: onConnect });
 
-	// Initialize new Paho client connection
-	client = new Paho.MQTT.Client(host, Number(port), clientID);
-
-	// Set callback handlers
-	client.onConnectionLost = onConnectionLost;
-	client.onMessageArrived = onMessageArrived;
-
-	// Connect the client, if successful, call onConnect function
-	client.connect({
-		onSuccess: onConnect
-	});
-}
-
-// Called when the client connects
+// called when the client connects
 function onConnect() {
-	// Fetch the MQTT topic from the form
-	topic = document.getElementById('topic').value;
-
-	// Print output for the user in the messages div
-	document.getElementById('messages').innerHTML += '<span>Subscribing to: ' + topic + '</span><br/>';
-
-	// Subscribe to the requested topic
+	// Once a connection has been made, make a subscription and send a message.
+	console.log('onConnect');
 	client.subscribe(topic);
+	message = new Paho.Message('Connection has been made, first message sent!');
+	message.destinationName = topic;
+	client.send(message);
 }
 
-// Called when the client loses its connection
+// called when the client loses its connection
 function onConnectionLost(responseObject) {
-	document.getElementById('messages').innerHTML += '<span>ERROR: Connection lost</span><br/>';
 	if (responseObject.errorCode !== 0) {
-		document.getElementById('messages').innerHTML += '<span>ERROR: ' + +responseObject.errorMessage + '</span><br/>';
+		console.log('onConnectionLost:' + responseObject.errorMessage);
 	}
 }
 
-// Called when a message arrives
+// called when a message arrives
 function onMessageArrived(message) {
-	console.log('onMessageArrived: ' + message.payloadString);
-	document.getElementById('messages').innerHTML += '<span>Topic: ' + message.destinationName + '  | ' + message.payloadString + '</span><br/>';
-}
-
-// Called when the disconnection button is pressed
-function startDisconnect() {
-	client.disconnect();
-	document.getElementById('messages').innerHTML += '<span>Disconnected</span><br/>';
+	console.log('onMessageArrived:' + message.payloadString);
 }

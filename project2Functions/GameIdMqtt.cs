2eb677fc69eeae1b5b1220b1413f9502cf84a9ab
Luka De Bakker <Luka.de.bakker@student.howest.be>
2020-01-08 20:24:43 +0100

    Added some more logging, checking if ID's are the right amount of digits

--- project2Functions/GameIdMqtt.cs
+++ project2Functions/GameIdMqtt.cs
@@ -21,29 +21,49 @@ namespace project2Functions
         public static void Run(
         [MqttTrigger("/luemniro/id/request")] IMqttMessage message, [Mqtt] out IMqttMessage outMessage, ILogger logger)
         {
+            // Creating Telemetry client for logging events!
             TelemetryClient telemetry = new TelemetryClient();
+            // Getting connection string
             telemetry.InstrumentationKey = Environment.GetEnvironmentVariable("insightsString");
+            // Receiving the message and creating an object from it
             var body = message.GetMessage();
             var bodyString = Encoding.UTF8.GetString(body);
             ID iD = JsonConvert.DeserializeObject<ID>(bodyString);
+            // Checking if the ID isn't already in the database
             bool IDResponse = InsertGameID.InsertId(iD.Id);
-            if (IDResponse)
+            // Checking if the ID is exactly 6 digits long
+            if(iD.Id.ToString().Length == 6)
             {
-                var response = new { id = iD.Id, status = "OK" };
-                var jsonResponse = JsonConvert.SerializeObject(response);
-                outMessage = new MqttMessage("/luemniro/id/response", Encoding.ASCII.GetBytes(jsonResponse), MqttQualityOfServiceLevel.AtLeastOnce, true);
-                logger.LogInformation("ID {iD.Id} is OK", iD.Id);
-                telemetry.TrackEvent("ID_Given");
-                telemetry.TrackEvent("ID_OK");
+                // If it's 6 digits long and NOT in the database
+                if (IDResponse)
+                {
+                    var response = new { id = iD.Id, status = "OK" };
+                    var jsonResponse = JsonConvert.SerializeObject(response);
+                    outMessage = new MqttMessage("/luemniro/id/response", Encoding.ASCII.GetBytes(jsonResponse), MqttQualityOfServiceLevel.AtLeastOnce, true);
+                    logger.LogInformation("ID {iD.Id} is OK", iD.Id);
+                    telemetry.TrackEvent("ID_Given");
+                    telemetry.TrackEvent("ID_OK");
+                }
+                // If the ID is already in the database
+                else
+                {
+                    var response = new { id = iD.Id, status = "NOK" };
+                    var jsonResponse = JsonConvert.SerializeObject(response);
+                    outMessage = new MqttMessage("/luemniro/id/response", Encoding.ASCII.GetBytes(jsonResponse), MqttQualityOfServiceLevel.AtLeastOnce, true);
+                    logger.LogInformation("ID {iD.Id} is NOK, ID already in database", iD.Id);
+                    telemetry.TrackEvent("ID_NOK");
+                }
             }
+            // If the ID is NOT 6 digits long!
             else
             {
                 var response = new { id = iD.Id, status = "NOK" };
                 var jsonResponse = JsonConvert.SerializeObject(response);
                 outMessage = new MqttMessage("/luemniro/id/response", Encoding.ASCII.GetBytes(jsonResponse), MqttQualityOfServiceLevel.AtLeastOnce, true);
-                logger.LogInformation("ID {iD.Id} is NOK, ID already in database", iD.Id);
+                logger.LogInformation("ID {iD.Id} is NOK, ID does not consist of exactly 6 digits", iD.Id);
                 telemetry.TrackEvent("ID_NOK");
             }
+            
         }
     }
 }

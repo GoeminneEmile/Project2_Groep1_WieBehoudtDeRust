53b143e14c37f690df2f46097d1bd8dfde620be4
Luka De Bakker <Luka.de.bakker@student.howest.be>
2020-01-12 12:42:03 +0100

    finished logging delete question

--- project2Functions/DeleteQuestion.cs
+++ project2Functions/DeleteQuestion.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System.Data.SqlClient;
+using Microsoft.ApplicationInsights;
 
 namespace project2Functions
 {
@@ -16,13 +17,20 @@ namespace project2Functions
         [FunctionName("DeleteQuestion")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+            ILogger logger)
         {
+            // Creating Telemetry client for logging events!
+            TelemetryClient telemetry = new TelemetryClient();
+            // Getting connection string.
+            telemetry.InstrumentationKey = Environment.GetEnvironmentVariable("insightsString");
+            // Take the data out of the reauest
             string guid = req.Query["guid"];
+            // Getting connection string
             string connectionString = Environment.GetEnvironmentVariable("connectionString");
             // opening SQL connection
             try
             {
+                // Opening SQL connection
                 using (SqlConnection connection = new SqlConnection())
                 {
                     // Setting connection string
@@ -42,6 +50,8 @@ namespace project2Functions
                         command.CommandText = "DELETE FROM ProjectAnswers WHERE QuestionAnswer = @questionguid";
                         command.Parameters.AddWithValue("@questionguid", guid);
                         var resultAnswer = await command.ExecuteReaderAsync();
+                        logger.LogInformation("Question with ID {ID} has been removed", guid);
+                        telemetry.TrackEvent("Delete_Question_OK");
                         return new OkObjectResult(201);
 
 
@@ -51,6 +61,8 @@ namespace project2Functions
             // catching an error
             catch (Exception ex)
             {
+                logger.LogInformation("Error with deleting question " + ex);
+                telemetry.TrackEvent("Delete_Question_NOK");
                 return new OkObjectResult(400);
             }
         }
